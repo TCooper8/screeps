48257280e9247d08950e12702e255778361c31c1
TCooper8 <coopetyl@live.com>
2017-12-15 14:03:24 -0700

    Updated room spawning

--- src/room.js
+++ src/room.js
@@ -15,17 +15,10 @@ const creepsInRoom =
 const isWorker = creep => creep.name[0] === 'W'
 const isGatherer = creep => creep.name[0] === 'G'
 
-const roomState = room => {
-  const creeps = Seq.ofObject(Game.creeps).map(pair => pair.value);
-  const roomCreeps = creepsInRoom(room)(creeps).cache();
-  const workers = Seq.filter(isWorker)(roomCreeps);
-  const gatherers = Seq.filter(isGatherer)(roomCreeps);
-
-  const sources = room.find(FIND_SOURCES_ACTIVE);
-
-  const sourcePositions =
+const availableWorkerSlots =
+  room =>
     Seq
-      .ofArray(sources)
+      .ofArray(room.find(FIND_SOURCES_ACTIVE))
       .map(source => {
         // Look at all the cells near this source.
         const objects = room.lookAtArea(
@@ -44,12 +37,34 @@ const roomState = room => {
       })
       .fold((count, array) => count + array.length)(0);
 
-  console.log("Worker position = ", sourcePositions);
+const createWorkers = room => nWorkers =>
+  // Go through any available spawners and create workers.
+  Seq
+    .ofObject(Game.spawns)
+    .filter(spawner => spawner.room.name === room.name)
+    .map(Worker.spawn)
+    .map(() => 1)
+    .sum()
+;
+
+const roomState = room => {
+  const creeps = Seq.ofObject(Game.creeps).map(pair => pair.value);
+  const roomCreeps = creepsInRoom(room)(creeps).cache();
+  const workers = Seq.filter(isWorker)(roomCreeps);
+  const gatherers = Seq.filter(isGatherer)(roomCreeps);
+
   console.log("Workers = ", workers.toArray().length)
 
   // Now, we can establish what work needs to be done.
 
   // How many slots for workers are there?
+  const workerSlots = availableWorkerSlots(room);
+  const workerCount = workers.count()
+  if (workerCount < workerSlots) {
+    // We need to create that many workers.
+    const workersSpawned = createWorkers(room)(workerSlots - workerCount);
+    console.log("Spawned", workersSpawned, "workers");
+  }
 }
 
 // We need to figure out what state the room is in.
