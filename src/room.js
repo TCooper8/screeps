09f10bfa9c5406634c8b980b1e2e516653199957
TCooper8 <coopetyl@live.com>
2017-12-15 13:51:23 -0700

    Updated room

--- src/room.js
+++ src/room.js
@@ -1,6 +1,7 @@
 // This module will handle the behavior for a given room.
 
 const Seq = require("./seq");
+const Array = require('./array');
 
 const economyState = 0
 
@@ -20,7 +21,36 @@ const roomState = room => {
   const workers = Seq.filter(isWorker)(roomCreeps);
   const gatherers = Seq.filter(isGatherer)(roomCreeps);
 
+  const sources = room.find(FIND_SOURCES_ACTIVE);
+
+  const sourcePositions =
+    Seq
+      .ofArray(sources)
+      .map(source => {
+        // Look at all the cells near this source.
+        const objects = room.lookAtArea(
+          source.pos.y - 1,
+          source.pos.x - 1,
+          source.pos.y + 1,
+          source.pos.x + 1,
+          true,
+        )
+        return
+          Seq
+            .ofArray(objects)
+            .filter(obj => obj.type === "terrain")
+            .filter(obj => obj.terrain !== "wall")
+            .toArray()
+      })
+      .fold((count, array) => count + array.length)(0);
+
+  console.log("Worker position = ", sourcePositions);
   console.log("Workers = ", workers.toArray().length)
+
+  // Now, we can establish what work needs to be done.
+
+  // How many slots for workers are there?
+  const workerSlots = 
 }
 
 // We need to figure out what state the room is in.
