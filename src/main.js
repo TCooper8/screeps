770633a2d836546038bb6067be8809aa6642e954
tcooper8 <coopetyl@live.com>
2017-12-14 20:26:51 -0700

    Update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
+const worker = require('./worker')
 
 module.exports.loop = function() {
+  const spawns = Object.keys(Game.spawns);
+  _.each(Game.spawns, (spawn) => {
+    if (spawn.energy >= worker.cost) {
+      worker.spawn(spawn);
+    }
+  });
   console.log("loop");
 }
---
+++ src/worker.js
@@ -0,0 +1,29 @@
+const moveCost = 50
+const workCost = 100
+const carryCost = 50
+
+const spawn = (spawner) => {
+  console.log("Spawning creep...");
+  const err = spawner.spawnCreep(
+    [ MOVE,
+      WORK,
+      CARRY,
+    ],
+    undefined,
+    undefined,
+  );
+  console.log("Spawned creep with error(%s)", err);
+}
+
+const cost = _.sum([
+  moveCost,
+  workCost,
+  carryCost,
+])
+
+const worker = {
+  spawn,
+  cost,
+}
+
+module.exports = worker;
