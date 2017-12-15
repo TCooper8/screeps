83e9d6869ceed594fb9a91442027ecf62b4fc1c4
TCooper8 <coopetyl@live.com>
2017-12-15 12:47:23 -0700

    Updated gatherer

--- src/gatherer.js
+++ src/gatherer.js
@@ -69,6 +69,9 @@ const returnResource = creep => {
       const err = creep.moveTo(creep.room.controller);
       console.log("Move to controller error %d", err);
     }
+    else {
+      creep.memory.state = gathering;
+    }
   }
 };
 
