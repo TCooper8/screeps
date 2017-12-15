3df1c56909dc65f746e94a7d0d13da85ca5ba4d6
TCooper8 <coopetyl@live.com>
2017-12-14 21:20:35 -0700

    Update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const worker = require('./worker')
-const Option = require('./option')
+const {
+  Option,
+} = require('./option')
 
 module.exports.loop = function() {
   const spawns = Object.keys(Game.spawns);
