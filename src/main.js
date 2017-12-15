4f2aa14afc819bdea4e55d680309b96ec3fae01c
TCooper8 <coopetyl@live.com>
2017-12-14 22:14:53 -0700

    Fixed code

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ const Option = require('./option')
 const Array = require('./array')
 
 const handleSpawners = Array.iter(spawner => {
-  console.log("Spawner %s", spawner.name);
-  if (spawn.energy >= worker.cost) {
-    worker.spawn(spawn);
+  if (spawner.energy >= Worker.cost) {
+    Worker.spawn(spawner);
   }
 });
 
